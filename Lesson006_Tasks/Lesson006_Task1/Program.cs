3ee0273e4de7c9631db2b36037723bb49e7a446a
Олександр Дибач <alex.dybach.2014@i.ua>
2020-11-22 16:22:58 +0200

    Done lesson006 tasks

---
+++ Lesson006_Tasks/Lesson006_Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson006_Task1
+{
+    class Program
+    {
+        static void CalculateIncrement(ref int a)
+        {
+            Console.WriteLine($"Increment = {++a}");
+        }
+        static void CalculateDecrement(ref int a)
+        {
+            Console.WriteLine($"Decrement = {--a}");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+
+            int number = int.Parse(Console.ReadLine());
+            CalculateIncrement(ref number);
+            Console.WriteLine($"number: {number}");
+
+            CalculateDecrement(ref number);
+            Console.WriteLine($"number: {number}");
+
+        }
+    }
+}
---
+++ Lesson006_Tasks/Lesson006_Task2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace Lesson006_Task2
+{
+    class Program
+    {
+        static void LogicOr(int a, int b)
+        {
+            Console.WriteLine($"LogicOr = {a | b}");
+        }
+        static void LogicAnd(int a, int b)
+        {
+            Console.WriteLine($"LogicAnd = {a & b}");
+        }
+        static void Exclusion(int a, int b)
+        {
+            Console.WriteLine($"Exclusion = {a ^ b}");
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Введіть два цілих числа:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть знак логічної операції:  ( |  &  ^ )");
+            string act = Console.ReadLine();
+
+            switch (act)
+            {
+                case ("|"):
+                    {
+                        LogicOr(a, b);
+                        break;
+                    }
+                case ("&"):
+                    {
+                        LogicAnd(a, b);
+                        break;
+                    }
+                case ("^"):
+                    {
+                        Exclusion(a, b);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Неправильний знак!");
+                        break;
+                    }
+            }
+            }
+    }
+}
